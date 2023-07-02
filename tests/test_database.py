43ad80c967dd72cb99ad01e3826075abb3340807
Matthew X. Economou <xenophon@irtnog.org>
2023-07-02 12:45:33 -0400

    test(database): verify integrity constraints and query results

--- tests/test_database.py
+++ tests/test_database.py
@@ -23,9 +23,11 @@ from psycopg2cffi import compat
 from pytest import fixture
 from pytest import mark
 from pytest import param
+from pytest import raises
 from sqlalchemy import create_engine
 from sqlalchemy import func
 from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import sessionmaker
 
 # invoke psycopg2cffi compatibility hook
@@ -71,3 +73,20 @@ def test_orm_basic(db_uri_fixture, request):
         stmt = select(func.count(System.name))
         cnt = session.scalars(stmt).one()
         assert cnt == 1
+
+    with raises(IntegrityError) as e_info:  # noqa: F841
+        with Session.begin() as session:
+            bad_system = System(
+                id64=0,
+                name='Bad System',
+                x=1.0,
+                y=2.0,
+                z=3.0,
+                date=datetime(1970, 1, 1, 0, 0),
+            )
+            session.add(bad_system)
+
+    with Session.begin() as session:
+        stmt = select(System).where(System.name.ilike('%test%'))
+        existing_system = session.scalars(stmt).one()
+        assert existing_system.name == 'Test System'
