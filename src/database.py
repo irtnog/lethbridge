aa14c94bb9a86a3e8aecbe524c489841443620ca
Matthew X. Economou <xenophon@irtnog.org>
2023-07-04 00:04:38 -0400

    refactor(database): remove unused import

--- src/database.py
+++ src/database.py
@@ -31,7 +31,6 @@ from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import relationship
-from sqlalchemy.orm import sessionmaker
 from typing import List
 from typing import Optional
 import logging
