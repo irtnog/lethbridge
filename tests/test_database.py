3abfc9a8638cbd7e54813674028038a1f26096e0
Matthew X. Economou <xenophon@irtnog.org>
2023-07-11 21:41:12 -0400

    test(database): revise and expand system data (de-)serialization checks

--- tests/test_database.py
+++ tests/test_database.py
@@ -45,18 +45,42 @@ def test_systemschema(mock_db_uri, mock_galaxy_dump):
             new_system = session.get(System, load_data.get("id64"))
             dump_data = SystemSchema().dump(new_system)
 
-        assert len(dump_data) <= len(load_data)
+        # compare keys; dump_data should be a subset of load_data
+        # until we finish the System class
+        assert set(dump_data) <= set(load_data)
+
+        # compare values; some keys require special handling
         for k in dump_data:
             if k == "powers":
-                # not clear whether Spansh sorts this
-                assert set(dump_data[k]) <= set(load_data[k])
+                assert set(dump_data[k]) == set(load_data[k])
+
+            # bodies
+
             elif k == "stations":
+                # dump_data should have the same number of spaceports
                 assert len(dump_data[k]) == len(load_data[k])
+
+                # compare each station
+                for dump_data_station, load_data_station in zip(
+                    sorted(dump_data[k], key=lambda st: st["name"]),
+                    sorted(load_data[k], key=lambda st: st["name"]),
+                ):
+                    # dump_data_station should be a subset of
+                    # load_data_station until we finish the Station
+                    # class
+                    assert set(dump_data_station) <= set(load_data_station)
+
             elif k == "date":
                 assert parse(dump_data[k]) == parse(load_data[k][:-3])
+
             else:
                 assert dump_data[k] == load_data[k]
 
+    # with the data loaded, make some queries
     with Session.begin() as session:
+        stmt = select(func.count(System.id64))
+        res = session.execute(stmt).first()
+        assert res[0] == 9
+
         fac = session.get(Faction, "Sol Workers' Party")
         assert len(fac.systems) == 4
