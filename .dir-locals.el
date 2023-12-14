9b6eeec374dc8980cde62bf20f7a20f4a4558a6b
Matthew X. Economou <xenophon@irtnog.org>
2023-12-13 20:57:06 -0500

    build: install or activate the development environment automatically in Emacs
    This requires enabling
    [`pyvenv-mode`](https://github.com/jorgenschaefer/pyvenv).

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,23 @@
-((python-mode . ((eval . (progn
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((eval . (progn
+                   ;; install or activate the development environment
+                   ;; (requires pyvenv-tracking-mode)
+                   (set (make-local-variable 'my-project)
+                        (locate-dominating-file default-directory ".dir-locals.el"))
+                   (set (make-local-variable 'my-project-venv)
+                        (concat my-project ".venv"))
+                   (if (not (file-exists-p my-project-venv))
+                       (let ((cwd default-directory))
+                         (cd my-project)
+                         (async-shell-command "make dev-infra")
+                         (cd cwd)
+                         (message "Please re-open this file/directory after the \"make dev-infra\" command finishes."))
+                     ;; must be set project-wide to pre-commit work
+                     (set (make-local-variable 'pyvenv-activate)
+                          my-project-venv))))))
+ (python-mode . ((eval . (progn
+                           ;; sort imports, then style code
                            (add-hook 'before-save-hook #'py-isort-before-save nil t)
                            (add-hook 'before-save-hook #'elpy-black-fix-code nil t))))))
